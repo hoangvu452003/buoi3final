c4becac3a502a798d96ebe1a902fa0f49ff8df9e
DuyVu2k3 <buitranduyvu2002@gmail.com>
2024-04-05 00:46:59 +0700

    first commit

---
+++ Buoi3/Repository/EFCategoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using Buoi3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buoi3.Repository
+{
+    public class EFCategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EFCategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+         public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+        public async Task AddAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        Task<IEnumerable<Category>> ICategoryRepository.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Category> ICategoryRepository.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
